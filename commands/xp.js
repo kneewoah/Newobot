9a15e8612bd3895bca9a90ab1ebc9d4129c1e66b
kneewoah <odogsimp13@gmail.com>
2020-10-16 22:39:35 -0400

    xp updates

--- commands/xp.js
+++ commands/xp.js
@@ -9,9 +9,24 @@ exports.run = (client, message, args, con) => {
 
     if(!rows[0]) return message.channel.send("imagine having 0 xp lol");
     let xp = rows[0].xp;
-    message.channel.send(xp);
+    var lvl = findLvl(xp);
+    message.channel.send(`Your total XP is: ${xp}. Your level is: ${lvl}.`);
   });
 
+}
+
+function findLvl(xp) {
+  for (int i = 1; i < 200; i++) {
+    if (xp < xpNeeded(i)) {
+      return i-1;
+}
+
+function xpNeeded(level) {
+  if (level = 0) return 0;
+  xpNeeded = 0;
+  for (var i = 0; i < level; i++) {
+    xpNeeded += 5*Math.pow((level-1),2)+50*(level-1)+100;
+  }
 }
 
 exports.help = {
