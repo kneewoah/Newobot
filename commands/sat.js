b4c790e8359dab6d2309de8aa970e84122ada55e
kneewoah <odogsimp13@gmail.com>
2020-10-17 14:34:29 -0400

    fix

--- commands/sat.js
+++ commands/sat.js
@@ -2,8 +2,6 @@ const config = require("../config.json");
 
 exports.run = (client, message, args) => {
 
-  if (args.length < 1) return message.channel.send("Please enter a school name");
-
   var score = Math.floor(Math.random()*121 + 40)*10;
 
   message.channel.send(`${message.author}, your next SAT score will be a ${score}.`);
