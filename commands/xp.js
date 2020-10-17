8ca51fa0c91aaeb0da406897c8b940122db56b69
kneewoah <odogsimp13@gmail.com>
2020-10-16 21:57:49 -0400

    fixed xp.js

--- commands/xp.js
+++ commands/xp.js
@@ -4,10 +4,10 @@ exports.run = (client, message, args, con) => {
 
   let target = message.author; //message.mentions.users.first() || message.guild.members.get(args[1]) ||
 
-  con.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+  con.query(`SELECT * FROM xp_${message.guild.id} WHERE id = '${target.id}'`, (err, rows) => {
     if(err) throw err;
 
-    if(!rows[0]) return message.channel.send("this user has no XP.");
+    if(!rows[0]) return message.channel.send("imagine having 0 xp lol");
     let xp = rows[0].xp;
     message.channel.send(xp);
   });
