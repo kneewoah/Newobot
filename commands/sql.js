0f8a2054aaf420eebb846d75946547c5c5e23ff4
kneewoah <odogsimp13@gmail.com>
2020-10-17 14:16:28 -0400

    fix

--- commands/sql.js
+++ commands/sql.js
@@ -3,9 +3,11 @@ const config = require("../config.json");
 exports.run = async (client, message, args, database) => {
 
   if (message.author.id !== config.ownerID) return message.channel.send("lol yea no.")
+  var query = args.join(' ');
+  if (query == "") return message.channel.send("u gotta put in a query bro")
 
   try {
-    database.query(args.join(' '));
+    database.query(query);
   } catch (e) {
     message.channel.send(e);
   }
