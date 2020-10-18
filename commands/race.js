6eda054f7fb8dad55bf7637113094bb6e44f1996
kneewoah <odogsimp13@gmail.com>
2020-10-18 19:12:26 -0400

    race

---
+++ commands/race.js
@@ -0,0 +1,26 @@
+const config = require("../config.json");
+
+exports.run = (client, message, args, con) => {
+
+  var msg = "";
+  let races = config.races;
+  let size = races.length;
+  var remaining = 100;
+
+  for (var i = 0; i < size, i++) {
+    var index = Math.floor(Math.random()*races.length);
+    var percent = Math.floor(Math.random()*remaining);
+    if (percent < 1) percent = "<1";
+    msg = msg + `${races[index]}: ${percent}%\n`;
+    races.splice(index, 1);
+    remaining -= percent;
+  }
+
+  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:\n\n` + msg);
+
+};
+
+exports.help = {
+  description: "WIP - XP tracker",
+  usage: "!xp"
+};
