86827fcafac0b66bc51b101488cd93559d399eda
kneewoah <odogsimp13@gmail.com>
2020-10-18 19:30:17 -0400

    clone

--- commands/race.js
+++ commands/race.js
@@ -3,7 +3,7 @@ const config = require("../config.json");
 exports.run = (client, message, args, con) => {
 
   var msg = "";
-  let races = config.races;
+  let races = Object.assign({}, config.races);
   let size = races.length;
   var remaining = 100;
 
@@ -11,9 +11,11 @@ exports.run = (client, message, args, con) => {
     var index = Math.floor(Math.random()*races.length);
     var percent = Math.floor(Math.random()*remaining);
     if (percent < 1) percent = "<1";
+    console.log(`Percent ${i+1}: ${percent});
     msg = msg + `${races[index]}: ${percent}%\n`;
     races.splice(index, 1);
     remaining -= percent;
+
   }
 
   message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:\n\n` + msg);
