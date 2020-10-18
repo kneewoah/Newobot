ea35d093249620086de60f8c6f0d6069d2a1666c
kneewoah <odogsimp13@gmail.com>
2020-10-18 19:45:42 -0400

    ``

--- commands/race.js
+++ commands/race.js
@@ -13,7 +13,7 @@ exports.run = (client, message, args, con) => {
     var percent2 = percent;
     if (percent < 1) percent2 = "<1";
     console.log(`Percent ${i+1}: ${percent}`);
-    msg = msg + `${races[index]}: ${percent2}%\n`;
+    msg = msg + `${races[index]}: \`${percent2}%\`\n`;
     races.splice(index, 1);
     remaining -= percent;
   }
