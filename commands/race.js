b431248ebc0d3a506deff278fcb8545aaa169a1f
kneewoah <odogsimp13@gmail.com>
2020-10-18 20:46:14 -0400

    SYNTAX

--- commands/race.js
+++ commands/race.js
@@ -26,7 +26,6 @@ exports.run = (client, message, args, con) => {
 
   percents.sort(function(a, b){return a-b});
   console.log(percents);
-  console.log(params);
   console.log(`https://quickchart.io/chart?c={type:'pie',data:{labels:${races},datasets:[{data:${percents}}]}}`);
 
   message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:`, {files: [`https://quickchart.io/chart?c={type:'pie',data:{labels:${races},datasets:[{data:${percents}}]}}`]});
