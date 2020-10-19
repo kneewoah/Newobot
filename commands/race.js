8cfab30200bfdfa38d1932a81044005c11dfc207
kneewoah <odogsimp13@gmail.com>
2020-10-18 21:34:39 -0400

    race2

--- commands/race.js
+++ commands/race.js
@@ -1,36 +1,24 @@
-const Discord = require('discord.js');
 const config = require("../config.json");
 
 exports.run = (client, message, args, con) => {
 
+  var msg = "";
   let races = JSON.parse(JSON.stringify(config.races));
   let size = races.length;
   var remaining = 100;
-  var percents = [];
 
   for (var i = 0; i < size; i++) {
+    var index = Math.floor(Math.random()*races.length);
     var percent = Math.floor(Math.random()*remaining);
     var percent2 = percent;
-    percents.push(percent2);
+    if (percent < 1) percent2 = "<1";
+    console.log(`Percent ${i+1}: ${percent}`);
+    msg = msg + `${races[index]}: \`${percent2}%\`\n`;
+    races.splice(index, 1);
     remaining -= percent;
   }
 
-  // sort
-  for (i = races.length -1; i > 0; i--) {
-    j = Math.floor(Math.random() * i)
-    k = races[i]
-    races[i] = races[j]
-    races[j] = k
-  }
-  console.log(JSON.stringify(races));
-
-  percents.sort(function(a, b){return a-b});
-  console.log(percents);
-  console.log(`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(races)},datasets:[{data:[${percents}]}]}}`);
-  const exampleEmbed = new Discord.RichEmbed()
-	.setImage(`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(races)},datasets:[{data:[${percents}]}]}}`);
-  message.channel.send(exampleEmbed);
-  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:`, {files: []});
+  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:\n\n` + msg);
 };
 
 exports.help = {
---
+++ commands/race2.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const config = require("../config.json");
+
+exports.run = (client, message, args, con) => {
+
+  let races = JSON.parse(JSON.stringify(config.races));
+  let size = races.length;
+  var remaining = 100;
+  var percents = [];
+
+  for (var i = 0; i < size; i++) {
+    var percent = Math.floor(Math.random()*remaining);
+    var percent2 = percent;
+    percents.push(percent2);
+    remaining -= percent;
+  }
+
+  // sort
+  for (i = races.length -1; i > 0; i--) {
+    j = Math.floor(Math.random() * i)
+    k = races[i]
+    races[i] = races[j]
+    races[j] = k
+  }
+  console.log(JSON.stringify(races));
+
+  percents.sort(function(a, b){return a-b});
+  console.log(percents);
+
+  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:`, {files: [`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(races)},datasets:[{data:[${percents}]}]}}`]});
+};
+
+exports.help = {
+  description: "DNA Analysis",
+  usage: "!race"
+};
