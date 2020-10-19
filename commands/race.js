0ddd6ca96fd21e15957f98fc56eaac204749f877
kneewoah <odogsimp13@gmail.com>
2020-10-18 20:20:29 -0400

    pie chart

--- commands/race.js
+++ commands/race.js
@@ -6,19 +6,37 @@ exports.run = (client, message, args, con) => {
   let races = JSON.parse(JSON.stringify(config.races));
   let size = races.length;
   var remaining = 100;
+  var percents = [];
 
   for (var i = 0; i < size; i++) {
-    var index = Math.floor(Math.random()*races.length);
     var percent = Math.floor(Math.random()*remaining);
-    var percent2 = percent;
-    if (percent < 1) percent2 = "<1";
     console.log(`Percent ${i+1}: ${percent}`);
-    msg = msg + `${races[index]}: \`${percent2}%\`\n`;
-    races.splice(index, 1);
+    var percent2 = percent;
+    if (percent < 1) percent2 = Math.random();
+    percents.push(percent2);
     remaining -= percent;
   }
 
-  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:\n\n` + msg);
+  for (i = races.length -1; i > 0; i--) {
+    j = Math.floor(Math.random() * i)
+    k = races[i]
+    races[i] = races[j]
+    races[j] = k
+  }
+
+  percents.sort(function(a, b){return a-b});
+
+  var params = {
+    type:'pie',
+    data: {
+      labels: races,
+      datasets: [{
+        data: percents
+      }]
+    }
+  }
+
+  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:`, {files: [`https://quickchart.io/chart?c=${params}`]});
 };
 
 exports.help = {
