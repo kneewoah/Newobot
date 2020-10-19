e8ed60c8323a0aecb6f5fdeaadde80b79c0f637f
kneewoah <odogsimp13@gmail.com>
2020-10-18 20:52:18 -0400

    brackets

--- commands/race.js
+++ commands/race.js
@@ -26,9 +26,10 @@ exports.run = (client, message, args, con) => {
 
   percents.sort(function(a, b){return a-b});
   console.log(percents);
-  console.log(`https://quickchart.io/chart?c={type:'pie',data:{labels:${races},datasets:[{data:${percents}}]}}`);
+  var url = `https://quickchart.io/chart?c={type:'pie',data:{labels:[${races}],datasets:[{data:[]${percents}]}]}}`
+  console.log(url);
 
-  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:`, {files: [`https://quickchart.io/chart?c={type:'pie',data:{labels:${races},datasets:[{data:${percents}}]}}`]});
+  message.channel.send(`${message.author}, after conducting DNA analysis, I have concluded your race to be as follows:`, {files: [url]});
 };
 
 exports.help = {
