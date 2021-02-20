7877a07c4fc888ae6bc9fc7d15dc4c5bfcc9f528
kneewoah <odogsimp13@gmail.com>
2021-02-20 17:29:18 -0500

    test2

--- commands/cum.js
+++ commands/cum.js
@@ -2,10 +2,11 @@ const config = require("../config.json");
 
 exports.run = async (client, message, args) => {
 
-  var randomPerson = await message.guild.members;
-  console.log(randomPerson);
+  var members = await message.guild.members.fetch();
+  var random[, {id}] = members.random().id;
+  console.log(random);
 
-  message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${randomPerson}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
+  message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${random}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
 
 };
 
