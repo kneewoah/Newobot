637cd16c7ddff8bad4c10d44398fd992a2bb7385
kneewoah <odogsimp13@gmail.com>
2021-02-20 18:11:14 -0500

    random

--- commands/cum.js
+++ commands/cum.js
@@ -5,7 +5,10 @@ exports.run = async (client, message, args) => {
   var members = await message.guild.members.cache;
   console.log(members);
 
-  message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${members.random().toString()}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
+  var random = members.random();
+  console.log(random);
+
+  message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${random}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
 
 };
 
