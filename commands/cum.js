2f3d9235c9b8567669fa4f836c6cd28a6f35d661
kneewoah <odogsimp13@gmail.com>
2021-02-20 17:20:56 -0500

    idk

--- commands/cum.js
+++ commands/cum.js
@@ -1,6 +1,6 @@
 const config = require("../config.json");
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 
   var randomPerson = await message.guild.members.fetch();
   console.log(randomPerson);
