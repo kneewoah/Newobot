0fed34f195035675bb220c5298268897c31dcaf8
kneewoah <odogsimp13@gmail.com>
2021-02-20 17:09:57 -0500

    test

--- commands/cum.js
+++ commands/cum.js
@@ -2,7 +2,7 @@ const config = require("../config.json");
 
 exports.run = (client, message, args) => {
 
-  var randomPerson = message.guild.members.random()
+  var randomPerson = message.guild.members.fetch().random()
   message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${randomPerson}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
 
 };
