f830046f337250d71896ebbb91fc3985020c09dd
kneewoah <odogsimp13@gmail.com>
2021-02-20 18:05:03 -0500

    undo

--- commands/cum.js
+++ commands/cum.js
@@ -2,7 +2,7 @@ const config = require("../config.json");
 
 exports.run = async (client, message, args) => {
 
-  var members = await message.guild.members.fetch().cache;
+  var members = await message.guild.members.cache;
   console.log(members);
 
   message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${members.random().toString()}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
