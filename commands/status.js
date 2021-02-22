ef2dbac46cd43ea51b4529c6d23f929a970f83af
kneewoah <odogsimp13@gmail.com>
2021-02-22 13:26:34 -0500

    fasdf

--- commands/status.js
+++ commands/status.js
@@ -3,8 +3,14 @@ const config = require("../config.json");
 exports.run = (client, message, args) => {
 
   if(message.author.id !== config.ownerID) return message.channel.send("You do not have permission to execute this command.");
-  client.user.setStatus(args[0]);
-  message.channel.send("My status has been updated");
+  var query = args[0].toLowercase();
+  if (query != 'online' || query != 'dnd' || query != 'idle' || query != 'invisible') {
+    return message.reply("Please enter a valid status: \`online | idle | dnd | invisible\`")
+  } else {
+    client.user.setStatus(args[0]);
+    message.channel.send("My status has been updated to + " args[0]);
+  }
+
 
 };
 
