2853f7c9cc0d36d25ba4dd90336038267be8ad2a
kneewoah <odogsimp13@gmail.com>
2021-02-22 13:49:43 -0500

    hmm

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -3,16 +3,21 @@ const config = require("../config.json");
 exports.run = (client, message, args) => {
 
   if(message.author.id !== config.ownerID) return message.channel.send("You do not have permission to execute this command.");
+  let style = args[0].toLowerCase();
+  if (style === "watching" || query === "streaming" ||  query === "listening" || query === "playing") {{
+    let text = args.slice(1).join(' ');
+    client.user.setActivity(text, { type: style });
+  } else {
+    let text = args.join(' ');
+    client.user.setActivity(text);
+  }
 
-  var query = args.join(' ');
 
-  client.user.setActivity(query);
-
-  message.channel.send("My activity has been updated to \'" + query + "\'");
+  message.channel.send("My activity has been updated");
 
 }
 
 exports.help = {
   description: "Change the actvity the bot is doing.",
-  usage: `${config.prefix}game [playing, listening, watching] <message>`
+  usage: `${config.prefix}setactivity [playing, listening, watching, streaming] <message>`
 };
