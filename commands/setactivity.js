4158cfdfedb29a4e52652bfb7e8fc7d0fc7156b8
kneewoah <odogsimp13@gmail.com>
2021-02-22 14:07:56 -0500

    blank

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -5,14 +5,14 @@ exports.run = (client, message, args) => {
   if(message.author.id !== config.ownerID) return message.channel.send("You do not have permission to execute this command.");
   let style = args[0].toUpperCase();
   let text;
-  if (style === "WATCHING" || style === "STREAMING" || style === "LISTENING" || style === "PLAYING") {
+  if (style === "WATCHING" || style === "LISTENING" || style === "PLAYING") {
     if (args[1]) {
       text = args.slice(1).join(' ');
       client.user.setActivity(text, { type: style });
       message.channel.send("My activity has been updated to " + style + " " + text);
     } else {
       client.user.setPresence({status: style });
-      message.channel.send("My status has been updated to " + style);
+      message.channel.send("My status has been updated to " + style + "Newo");
     }
   } else {
     text = args.join(' ');
