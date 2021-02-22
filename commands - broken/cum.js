032f955847dd5b966fdd08a5026579bfb62a1bd4
kneewoah <odogsimp13@gmail.com>
2021-02-22 12:39:29 -0500

    test

--- commands - broken/cum.js
+++ commands - broken/cum.js
@@ -2,13 +2,11 @@ const config = require("../config.json");
 
 exports.run = async (client, message, args) => {
 
-  var members = await message.guild.members.fetch.then(console.log).catch(console.error).cache;
-  console.log(members);
+  var members = await message.guild.members;
+  const users = members.users.cache;
+  console.log(users);
 
-  var random = members.random();
-  console.log(random);
-
-  message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${random}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
+  message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${users.random()}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
 
 };
 
