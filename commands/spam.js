0b0647a601a993cab5e615906a099c71866272e2
kneewoah <odogsimp13@gmail.com>
2021-02-22 16:23:10 -0500

    spam

---
+++ commands/spam.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const config = require("../config.json");
+
+exports.run = (client, message, args, con) => {
+  var target = message.mentions.members.first());
+
+  for (var i = 0; i < 10; i++) {
+    message.channel.send(target.toString());
+  }
+};
+
+exports.help = {
+  description: "spam anyone",
+  usage: "!spam <user>"
+};
