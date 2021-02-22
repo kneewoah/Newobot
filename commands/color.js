f0c4cebb98a131c499e91bf14796861b715b2f07
kneewoah <odogsimp13@gmail.com>
2021-02-22 17:07:50 -0500

    fgr

--- commands/color.js
+++ commands/color.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args) => {
   console.log("Editing color for " + mAuthor.id);
 
   if (args[0].toLowerCase() === "random") {
-    changeColor(Math.floor(Math.random()*16777215).toString(16));
+    changeColor(Math.floor(Math.random()*16777215).toString(16), mAuthor);
   } else if (args[0].match(/^#(?:[0-9a-fA-F]{6})$/g)) {
     changeColor(args[0].substring(1), mAuthor);
   } else if (args[0].match(/^(?:[0-9a-fA-F]{6})$/g)) {
