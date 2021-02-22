444a5b80fe9d7df7605be4cb175c169c2017bc02
kneewoah <odogsimp13@gmail.com>
2021-02-22 14:08:20 -0500

    oops

--- commands/live.js
+++ commands/live.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
 
   if(message.author.id !== config.ownerID) return message.channel.send("You do not have permission to execute this command.");
 
-  client.user.setActivity("Destiny 2", {url: "https://twitch.tv/newox", type: 'PLAYING'});
+  client.user.setActivity("Destiny 2", {url: "https://twitch.tv/newox", type: 'STREAMING'});
 
 }
 
