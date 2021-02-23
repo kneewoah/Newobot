ebcb33a1acef15e101d155f50df6c6daf089a878
kneewoah <odogsimp13@gmail.com>
2021-02-22 19:03:36 -0500

    test

--- modules/reactions.js
+++ modules/reactions.js
@@ -11,22 +11,22 @@ exports.add = (client, message, user, reaction, emoji) => {
       var yes = embed.fields[0];
       var no = embed.fields[1];
 
-      embed.spliceFields(0);
+      embed.spliceFields(0, 2);
 
       embed.addField({name: "Yes", value: yes.value + " " + user.toString(), inline: true})
       embed.addField(no);
+      message.edit(embed);
       reaction.remove(user);
   }
 
   else if(emoji.name == '❌') {
     var embed = message.embeds[0];
-    var yes = embed.fields[0];
     var no = embed.fields[1];
 
-    embed.spliceFields(0);
+    embed.spliceFields(1, 1);
 
-    embed.addField(yes)
     embed.addField({name: "No", value: no.value + " " + user.toString(), inline: true});
+    message.edit(embed);
     reaction.remove(user);
   }
 
