86efd59b46e196230938f419e7c0b37c8996c8fc
kneewoah <odogsimp13@gmail.com>
2021-02-22 22:08:56 -0500

    asgf

--- commands/lb.js
+++ commands/lb.js
@@ -6,6 +6,7 @@ exports.run = (client, message, args, con) => {
   con.query(`SELECT * FROM xp_${message.guild.id}`, (err, data) => {
     if(err) throw err;
 
+    consol.log(data);
     const sorted = data.sort((a, b) => (a.xp > b.xp ? -1 : 1));
 
     var color = message.guild.roles.cache.find(role => role.name === message.author.id).color.toString(16);
@@ -21,6 +22,7 @@ exports.run = (client, message, args, con) => {
     });
 
     console.log(sorted);
+
     var i = 0;
     const cache = message.guild.members.cache;
     sorted.forEach(entry => {
