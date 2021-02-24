4af22af9b063c908248ac5eaea54fba3cefcd905
kneewoah <odogsimp13@gmail.com>
2021-02-24 15:44:08 -0500

    chanceme

--- commands/chanceme.js
+++ commands/chanceme.js
@@ -2,13 +2,16 @@ const config = require("../config.json");
 
 exports.run = (client, message, args) => {
 
-  if (args.length < 1) return message.channel.send("Please enter a school name");
+  if (args.length < 1) return message.channel.send("Please enter a school name")
+  .then(message => console.log(`Sent message: ${message.content}`))
+  .catch(console.error);
 
   var chance = Math.floor(Math.random()*101)
-
   var school = args.join(" ");
 
-  message.channel.send(`${message.author}, you have a ${chance} percent chance at getting into ${school}.`);
+  message.channel.send(`${message.author}, you have a ${chance} percent chance at getting into ${school}.`)
+  .then(message => console.log(`Sent message: ${message.content}`))
+  .catch(console.error);
 
 };
 
