db9fbe323e1fcf222f058014149055b22343d4a1
kneewoah <odogsimp13@gmail.com>
2021-02-23 23:27:31 -0500

    clean

--- modules/reactions.js
+++ modules/reactions.js
@@ -4,49 +4,49 @@ const fs = require("fs");
 
 exports.add = (client, message, user, reaction, emoji, database) => {
 
-  if(message.author !== client.user)  return;
-  if(message.channel.id !== config.scrimChannel) return;
+  if((message.author === client.user) && (message.channel.id === config.scrimChannel))  {
+    if(emoji.name == '✅') {
+        var embed = message.embeds[0];
+        // Type: MessageEmbed
 
-  if(emoji.name == '✅') {
+        var yes = embed.fields[0];
+        var no = embed.fields[1];
+        // Type EmbedField
+
+        embed.spliceFields(0, 2);
+
+        var count = parseInt(yes.value.substring(0,1), 10);
+        count++;
+
+        embed.addField("Yes", count + yes.value.substring(1).replace(user.toString(), "") + user.toString(), true);
+        embed.addField("No", no.value.replace(user.toString(), ""), true);
+
+        message.edit(embed);
+        reaction.users.remove(user);
+
+        message.guild.channels.cache.get(config.pillowsGeneralID).send(user.toString() + " can scrim.");
+        
+    } else if(emoji.name == '❌') {
       var embed = message.embeds[0];
-      // Type: MessageEmbed
-
       var yes = embed.fields[0];
       var no = embed.fields[1];
-      // Type EmbedField
 
       embed.spliceFields(0, 2);
 
-      var count = parseInt(yes.value.substring(0,1), 10);
+      var count = parseInt(no.value.substring(0,1), 10);
       count++;
 
-      embed.addField("Yes", count + yes.value.substring(1).replace(user.toString(), "") + user.toString(), true);
-      embed.addField("No", no.value.replace(user.toString(), ""), true);
+      embed.addField("Yes", yes.value.replace(user.toString(), ""), true);
+      embed.addField("No", count + no.value.substring(1).replace(user.toString(), "") + user.toString(), true);
 
       message.edit(embed);
       reaction.users.remove(user);
 
-      message.guild.channels.cache.get(config.pillowsGeneralID).send(user.toString() + " can scrim.");
+      message.guild.channels.cache.get(config.pillowsGeneralID).send(user.toString() + " cannot scrim.");
+    }
   }
 
-  else if(emoji.name == '❌') {
-    var embed = message.embeds[0];
-    var yes = embed.fields[0];
-    var no = embed.fields[1];
 
-    embed.spliceFields(0, 2);
-
-    var count = parseInt(no.value.substring(0,1), 10);
-    count++;
-
-    embed.addField("Yes", yes.value.replace(user.toString(), ""), true);
-    embed.addField("No", count + no.value.substring(1).replace(user.toString(), "") + user.toString(), true);
-
-    message.edit(embed);
-    reaction.users.remove(user);
-
-    message.guild.channels.cache.get(config.pillowsGeneralID).send(user.toString() + " cannot scrim.");
-  }
 
 };
 
