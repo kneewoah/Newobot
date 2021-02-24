50b225a537b777e206986afe19a2023cf7c48385
kneewoah <odogsimp13@gmail.com>
2021-02-24 16:41:05 -0500

    test

--- modules/roleColor.js
+++ modules/roleColor.js
@@ -13,7 +13,7 @@ exports.run = (client, message, database, member) => {
       member.roles.add(existingRole, `Someone removed this role from ${member}`);
 
     } else {
-      var newRole = message.guild.roles.create({
+      message.guild.roles.create({
         data: {
           name: `${roleName}`,
           color: `0x${Math.floor(Math.random()*16777215).toString(16)}`,
@@ -22,10 +22,10 @@ exports.run = (client, message, database, member) => {
         },
         reason: `Default color for ${member.username}`
       })
-      //.then(r => console.log(`Created role '${r}'.`))
-      //.catch(console.error);
+      .then(r => console.log(`Created role '${r}'.`))
+      .catch(console.error);
 
-      message.member.roles.add(newRole, "Newo Bot")
+      message.member.roles.add(roleName, "Newo Bot")
       .then(u => console.log(`Added role '${newRole.name}' to ${u}.`))
       .catch(console.error);
     };
