7934b3da3244b90f71a0ed5a98e927ec4b884108
kneewoah <odogsimp13@gmail.com>
2021-02-24 17:50:00 -0500

    syn

--- modules/roleColor.js
+++ modules/roleColor.js
@@ -10,8 +10,7 @@ exports.run = (client, message, database, member) => {
     // Apply old color
     var existingRole = member.guild.roles.cache.find(role => role.name === roleName);
     if (existingRole.members.find(mem => mem.id === roleName) return;
-
-    if (existingRole) {
+    else if (existingRole) {
       member.roles.add(existingRole, `Someone removed this role from ${member}`)
       .then(u => console.log(`Fixed missing role. Added role '${existingRole.name}' to ${u.user.tag}.`))
       .catch(console.error);
