64c943c5cd671bd8fedbb9648d22f0efc417932f
kneewoah <odogsimp13@gmail.com>
2021-02-24 20:33:03 -0500

    lol

--- commands/lb.js
+++ commands/lb.js
@@ -21,7 +21,7 @@ exports.run = (client, message, args, con) => {
     });
 
     for (var i = 0; i < sorted.length; i++) {
-      var user = message.guild.members.get(sorted[i].id).user
+      var user = message.guild.members.cache.get(sorted[i].id).user
       embed.addField(`${i+1}. ${user.toSrting()}`, `**XP:** ${sorted[i].xp}\n**Level:** ${sorted[i].level}`, true);
     }
 
