1cf02eb523191dceb724868ab35e6cc4d23e2344
kneewoah <odogsimp13@gmail.com>
2021-02-25 12:28:37 -0500

    .

--- commands/color.js
+++ commands/color.js
@@ -24,7 +24,7 @@ exports.run = async (client, message, args, database) => {
   }
 
   function changeColor(color, author) {
-    const role = require(`../modules/roleColor.js`).run(client, message, database, member, color);
+    const role = require(`../modules/roleColor.js`).run(client, message, database, author, color);
     role.edit({color: `0x${color}`})
     .then(updated => console.log(`Edited role color for ${author.tag} to ${updated.color}`))
     .catch(console.error);
