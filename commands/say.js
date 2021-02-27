5e8c999d2592be1c71ddc957d31cf97593730038
kneewoah <odogsimp13@gmail.com>
2021-02-26 19:48:03 -0500

    !

--- commands/say.js
+++ commands/say.js
@@ -3,15 +3,15 @@ const config = require("../config.json");
 exports.run = (client, message, args) => {
 
   const sayMessage = args.join(" ");
-  const chan = message.channel
+  const chan = message.channel;
+
+  message.channel.send(sayMessage)
+  .then(msg => console.log(`Echo'd message from ${msg.author.tag}: ${msg.content}`))
+  .catch(console.error);
 
   message.delete()
     .then(msg => console.log(`Deleted message from ${msg.author.tag}`))
     .catch(console.error);
-
-  chan.send(sayMessage)
-  .then(msg => console.log(`Echo'd message from ${msg.author.tag}: ${msg.content}`))
-  .catch(console.error);
 
 };
 
