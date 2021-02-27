79bcf503532365a1331ba33ce120f4d00039872f
kneewoah <odogsimp13@gmail.com>
2021-02-27 01:55:22 -0500

    !

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,12 @@ exports.run = async (client, message, args) => {
   const m = await message.channel.send("Ping?")
     .then(m => {
       console.log(`Sent message: ${m.content}`);
-      m.edit(`Pong! Latency is ${m.createdTimestamp - m.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+      m.edit(`Pong! Latency is ${m.createdTimestamp - m.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+      .then(msg => console.log(`Updated the content of a message to ${msg.content}`))
+      .catch(console.error);
     })
     .catch(console.error);
 
-  .then(msg => console.log(`Updated the content of a message to ${msg.content}`))
-  .catch(console.error);
 
 };
 
