105d6dd102b21bf431f655e1a1f2fb2896dded52
kneewoah <odogsimp13@gmail.com>
2021-02-28 23:02:57 -0500

    leave

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ client.on('ready', () => {
   console.log("Newo Bot is Online");
   console.log(`Owner ID: ${config.ownerID}`);
 
+  client.guilds.cache.get("459894790654263336").leave();
   console.log(client.guilds.cache);
 
   client.user.setActivity("Destiny 2", {url: "https://twitch.tv/newox", type: 'PLAYING'})
