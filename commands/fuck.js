92ed0ed8a43cb1755e9714d38b561e088aa048e2
kneewoah <odogsimp13@gmail.com>
2021-03-01 15:05:34 -0500

    d

--- commands/fuck.js
+++ commands/fuck.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
   const members = await message.guild.members.cache.filter(guildMember => !guildMember.user.bot);
   const person = members.random().user;
 
-  message.channel.send(`${message.author} whips out his dick and fucks ${person}in the asshole. \n:peach::eggplant:`)
+  message.channel.send(`${message.author} whips out his dick and fucks ${person} in the asshole. \n:peach::eggplant:`)
     .then(message => console.log(`Sent message: ${message.content}`))
     .catch(console.error);
 
