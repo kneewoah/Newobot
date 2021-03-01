708da4cc929106caf4f70e0ac76177fe817d3ac0
kneewoah <odogsimp13@gmail.com>
2021-03-01 12:52:40 -0500

    id

--- modules/handleXP.js
+++ modules/handleXP.js
@@ -80,9 +80,9 @@ exports.text = (client, message, database) => {
 
 exports.voice = (client, oldVoiceState, newVoiceState, database) => {
 
-  if (((newVoiceState.channel == null && !oldVoiceState.deaf && oldVoiceState.channel !== null) || (newVoiceState.deaf && newVoiceState.channel == oldVoiceState.channel) || (oldVoiceState.channel !== newVoiceState.channel && !oldVoiceState.deaf && oldVoiceState.channel !== null)) && !((oldVoiceState.channel == null && oldVoiceState.channel.id === config.pillowsAFK) || oldVoiceState.guild.id !== config.pillowsID || (oldVoiceState.channel === null && newVoiceState.channel.id === config.pillowsAFK) || (oldVoiceState.channel === config.pillowsAFK && newVoiceState.channel !== config.pillowsAFK && !oldVoiceState.deaf))) { // END XP COUNT
+  if (((newVoiceState.channel == null && !oldVoiceState.deaf && oldVoiceState.channel !== null) || (newVoiceState.deaf && newVoiceState.channel == oldVoiceState.channel) || (oldVoiceState.channel !== newVoiceState.channel && !oldVoiceState.deaf && oldVoiceState.channel !== null)) && !((oldVoiceState.channel == null && oldVoiceState.channel.id === config.pillowsAFK) || oldVoiceState.guild.id !== config.pillowsID || (oldVoiceState.channel === null && newVoiceState.channel.id === config.pillowsAFK) || (oldVoiceState.channel.id === config.pillowsAFK && newVoiceState.channel.id !== config.pillowsAFK && !oldVoiceState.deaf))) { // END XP COUNT
 
-    if(!newVoiceState.deaf && !oldVoiceState.deaf && oldVoiceState.channel !== null && newVoiceState.channel !== null && oldVoiceState.channel !== newVoiceState.channel && newVoiceState.channel !== config.pillowsAFK) return console.log(newVoiceState.channel !== config.pillowsAFK);
+    if(!newVoiceState.deaf && !oldVoiceState.deaf && oldVoiceState.channel !== null && newVoiceState.channel !== null && oldVoiceState.channel !== newVoiceState.channel && newVoiceState.channel.id !== config.pillowsAFK) return console.log(newVoiceState.channel.id !== config.pillowsAFK);
 
     console.log(`UPDATING voice channel XP for ${oldVoiceState.member.user.tag}`);
     database.query(`SELECT * FROM xp_${config.pillowsID} WHERE id = '${newVoiceState.member.id}'`, (err, data) => {
