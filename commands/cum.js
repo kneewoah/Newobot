4c7f43b73627d9cf8f17062fb9a0ea0f88c09903
kneewoah <odogsimp13@gmail.com>
2021-02-28 22:32:11 -0500

    yup

--- commands/cum.js
+++ commands/cum.js
@@ -2,7 +2,7 @@ const config = require("../config.json");
 
 exports.run = async (client, message, args) => {
 
-  const members = await message.guild.members.fetch().filter(guildMember => !guildMember.user.bot);
+  const members = await message.guild.members.cache.filter(guildMember => !guildMember.user.bot);
 
   message.channel.send(`${message.author} just pulled out his dick and decided to cum on ${members.random().user.toString()}. The cum flew ${Math.floor((Math.random())*42)+6} inches across the room. \n:weary::fist::eggplant::sweat_drops:`)
     .then(message => console.log(`Sent message: ${message.content}`))
