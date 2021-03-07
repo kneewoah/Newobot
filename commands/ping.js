f06e8dc2d31f712bb8c5b897b23c95392c4d968d
kneewoah <42323086+kneewoah@users.noreply.github.com>
2021-03-07 08:23:49 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,12 @@ const config = require("../config.json");
 
 exports.run = async (client, message, args) => {
 
-  const m = await message.channel.send("Ping?")
+  const message = await message.channel.send("Ping?")
     .then(m => {
       console.log(`Sent message: ${m.content}`);
-      m.edit(`Pong! Latency is ${m.createdTimestamp - m.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
-      .then(msg => console.log(`Updated the content of a message to ${msg.content}`))
-      .catch(console.error);
+      message.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+        .then(msg => console.log(`Updated the content of a message to ${msg.content}`))
+        .catch(console.error);
     })
     .catch(console.error);
 
